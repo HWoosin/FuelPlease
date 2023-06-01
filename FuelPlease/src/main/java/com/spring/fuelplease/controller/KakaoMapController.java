be630f11b27a1c726a34c41321e39dc4e0f56522
woosin <ws1340cc@naver.com>
2023-06-01 20:23:32 +0900

    주유소 위치와 전기차충전소 위치 검색 기능 구현 230601

--- FuelPlease/src/main/java/com/spring/fuelplease/controller/KakaoMapController.java
+++ FuelPlease/src/main/java/com/spring/fuelplease/controller/KakaoMapController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.spring.fuelplease.AllFuel.service.IElecService;
 import com.spring.fuelplease.AllFuel.service.IGasolineService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,26 +23,51 @@ public class KakaoMapController {
 	
 	@Autowired
 	private IGasolineService gsv;
+	
+	@Autowired
+	private IElecService esv;
+	
+	@GetMapping("/inputGasoline")
+	public void inputG() {
+		log.info("주유소 정보 주입");
+		gsv.inputGasoline();
+	}
+	
+	@GetMapping("/inputElec")
+	public void inputE() {
+		log.info("전기차 정보 주입");
+		esv.inputElec();
+	}
 
+	//주유소
 	@GetMapping("/gasolineMap")
-	public void goMap() {
-		log.info("카카오 맵으로~");
+	public void findGasoline() {
+		log.info("주유소 맵으로~");
 	}
 	
 	//~~구의 도로명들 가져오기
 	@PostMapping("/gasolineMap")
 	@ResponseBody
-	public List<String> findGasoline(@RequestBody String keyword, Model model) {
+	public List<String> findGasoline(@RequestBody String keyword) {
 		List<String> spot = gsv.findGasoline(keyword);
 //		log.info(spot);
 //		model.addAttribute("spot",spot);
 		return spot;
 	}
 	
-	@GetMapping("/inputGasoline")
-	public void inputG() {
-		log.info("주유소 정보 주입");
-		gsv.inputGasoline();
+	///전기차
+	@GetMapping("/elecChargeMap")
+	public void findElec() {
+		log.info("전기차 충전소 맵으로~");
+	}
+	
+	@PostMapping("/elecChargeMap")
+	@ResponseBody
+	public List<String> findElec(@RequestBody String keyword) {
+		List<String> spot = esv.findElec(keyword);
+//		log.info(spot);
+//		model.addAttribute("spot",spot);
+		return spot;
 	}
 	
 }

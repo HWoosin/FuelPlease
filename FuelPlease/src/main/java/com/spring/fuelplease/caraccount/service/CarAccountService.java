bf6bdb2b94c42db1b0f2b02175d90ab222f56e6e
ChangjuMun <changju7857@gmail.com>
2023-06-05 09:40:53 +0900

    23.06.05 풀리퀘커밋

--- FuelPlease/src/main/java/com/spring/fuelplease/caraccount/service/CarAccountService.java
+++ FuelPlease/src/main/java/com/spring/fuelplease/caraccount/service/CarAccountService.java
@@ -1,6 +1,6 @@
 package com.spring.fuelplease.caraccount.service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,21 +14,22 @@ public class CarAccountService implements ICarAccountService {
 	@Autowired
 	private ICarAccountMapper mp;
 	
-	@Override
-	public ArrayList<String> calendarList() {
-		return null;
-	}
-
-	@Override
-	public void deleteAccount(carAccountVO vo) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	// 등록
 	@Override
 	public void addAccount(carAccountVO vo) {
-		mp.addAccount(vo);
+		mp.addAccount(vo);		
+	}
+	
+	@Override
+	public List<String> calendarList() {
+		return null;
+	}
+	
+	// 삭제
+	@Override
+	public void deleteAccount(carAccountVO vo) {
 		
 	}
+
 
 }
--- FuelPlease/src/main/java/com/spring/fuelplease/caraccount/service/ICarAccountService.java
+++ FuelPlease/src/main/java/com/spring/fuelplease/caraccount/service/ICarAccountService.java
@@ -6,14 +6,15 @@ import com.spring.fuelplease.voCenter.carAccountVO;
 
 public interface ICarAccountService {
 
+	// 등록하기	
+	void addAccount(carAccountVO vo);
+	
 	// 목록 가져오기
 	List<String> calendarList();
 	
 	// 삭제하기
 	void deleteAccount(carAccountVO vo);
 	
-	// 등록하기	
-	void addAccount(carAccountVO vo);
 	
 	
 }
